23a5cad31ffcec44f8dbd509ef635a86485dcdd0
Egor Rybin <107394961+EgorRybin@users.noreply.github.com>
2024-02-01 12:18:10 +0200

    update css

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import { useParams, Link } from "react-router-dom";
 import { getElementById } from "../../API/API";
 import PhotoSwipe from "../PhotoSwipe/PhotoSwipe";
-import arrowSvg from "../../img/arrow-left2.svg";
+// import arrowSvg from "../../img/arrow-left2.svg";
 import css from "./ProductDetail.module.css";
 
 
@@ -14,7 +14,10 @@ const ProductDetail = ({ data }) => {
     <div className={css.wrapper}>
       <div className={css.border}></div>
       <Link to={"/"}>
-        <img src={arrowSvg} alt="arrow" />
+      <svg className={css.arrowLink} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+       <title>arrow-left2</title>
+       <path d="M12.586 27.414l-10-10c-0.781-0.781-0.781-2.047 0-2.828l10-10c0.781-0.781 2.047-0.781 2.828 0s0.781 2.047 0 2.828l-6.586 6.586h19.172c1.105 0 2 0.895 2 2s-0.895 2-2 2h-19.172l6.586 6.586c0.39 0.39 0.586 0.902 0.586 1.414s-0.195 1.024-0.586 1.414c-0.781 0.781-2.047 0.781-2.828 0z"></path>
+      </svg>
       </Link>
       <h2 className={css.title}>{products.title}</h2>
       <p className={css.description}>{products.fullPreDescription}</p>
